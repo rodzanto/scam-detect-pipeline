358c61db2e4fe16a557b17166bfdcd3c631c9c04
Antonio Rodriguez <42835728+rodzanto@users.noreply.github.com>
2021-09-01 17:09:48 +0200

    Add files via upload

---
+++ pipelines/pipeline.py
@@ -0,0 +1,327 @@
+"""Workflow pipeline script for scam-detect pipeline.
+                                               . -RegisterModel
+                                              .
+    Process-> Train -> Evaluate -> Condition .
+                                              .
+                                               . -(stop)
+Implements a get_pipeline(**kwargs) method.
+"""
+import os, time, json
+import boto3
+import sagemaker
+from sagemaker.estimator import Estimator
+from sagemaker.inputs import TrainingInput
+from sagemaker.processing import (
+    ProcessingInput,
+    ProcessingOutput,
+    Processor,
+    ScriptProcessor,
+)
+from sagemaker import Model
+from sagemaker.model_metrics import MetricsSource, ModelMetrics
+from sagemaker.xgboost import XGBoostPredictor
+from sagemaker.sklearn.processing import SKLearnProcessor
+from sagemaker.workflow.parameters import ParameterInteger, ParameterString
+from sagemaker.workflow.pipeline import Pipeline
+from sagemaker.workflow.properties import PropertyFile
+from sagemaker.workflow.steps import ProcessingStep, TrainingStep, CacheConfig
+from sagemaker.workflow.lambda_step import (
+    LambdaStep,
+    LambdaOutput,
+    LambdaOutputTypeEnum,
+)
+from sagemaker.workflow.step_collections import CreateModelStep, RegisterModel
+from sagemaker.workflow.conditions import ConditionLessThanOrEqualTo
+from sagemaker.workflow.condition_step import ConditionStep, JsonGet
+from sagemaker.lambda_helper import Lambda
+
+BASE_DIR = os.path.dirname(os.path.realpath(__file__))
+
+def get_sagemaker_client(region):
+     """Gets the sagemaker client.
+
+        Args:
+            region: the aws region to start the session
+            default_bucket: the bucket to use for storing the artifacts
+
+        Returns:
+            `sagemaker.session.Session instance
+        """
+     boto_session = boto3.Session(region_name=region)
+     sagemaker_client = boto_session.client("sagemaker")
+     return sagemaker_client
+
+
+def get_session(region, default_bucket):
+    """Gets the sagemaker session based on the region.
+
+    Args:
+        region: the aws region to start the session
+        default_bucket: the bucket to use for storing the artifacts
+
+    Returns:
+        `sagemaker.session.Session instance
+    """
+
+    boto_session = boto3.Session(region_name=region)
+
+    sagemaker_client = boto_session.client("sagemaker")
+    runtime_client = boto_session.client("sagemaker-runtime")
+    return sagemaker.session.Session(
+        boto_session=boto_session,
+        sagemaker_client=sagemaker_client,
+        sagemaker_runtime_client=runtime_client,
+        default_bucket=default_bucket,
+    )
+
+def get_pipeline_custom_tags(new_tags, region, sagemaker_project_arn=None):
+    try:
+        sm_client = get_sagemaker_client(region)
+        response = sm_client.list_tags(
+            ResourceArn=sagemaker_project_arn)
+        project_tags = response["Tags"]
+        for project_tag in project_tags:
+            new_tags.append(project_tag)
+    except Exception as e:
+        print(f"Error getting project tags: {e}")
+    return new_tags
+
+
+def get_pipeline(
+    region,
+    sagemaker_project_arn=None,
+    role=None,
+    default_bucket=None,
+    model_package_group_name="scam-detect-package-group",
+    pipeline_name="scam-detect-pipeline",
+    base_job_prefix="scam-detect",
+):
+    """Gets a SageMaker ML Pipeline instance working.
+
+    Args:
+        region: AWS region to create and run the pipeline.
+        role: IAM role to create and run steps and pipeline.
+        default_bucket: the bucket to use for storing the artifacts
+
+    Returns:
+        an instance of a pipeline
+    """
+    sagemaker_session = get_session(region, default_bucket)
+    if role is None:
+        role = sagemaker.session.get_execution_role(sagemaker_session)
+
+    # parameters for pipeline execution
+    processing_instance_count = ParameterInteger(name="ProcessingInstanceCount", default_value=1)
+    processing_instance_type = ParameterString(
+        name="ProcessingInstanceType", default_value="ml.m5.xlarge"
+    )
+    training_instance_type = ParameterString(
+        name="TrainingInstanceType", default_value="ml.m5.xlarge"
+    )
+    model_approval_status = ParameterString(
+        name="ModelApprovalStatus", default_value="PendingManualApproval"
+    )
+    input_data = ParameterString(
+        name="InputDataUrl",
+        default_value=f"s3://sagemaker-servicecatalog-seedcode-{region}/dataset/abalone-dataset.csv",
+    )
+
+    # processing step for feature engineering
+    sklearn_processor = SKLearnProcessor(
+        framework_version="0.23-1",
+        instance_type=processing_instance_type,
+        instance_count=processing_instance_count,
+        base_job_name=f"{base_job_prefix}/preprocess",
+        sagemaker_session=sagemaker_session,
+        role=role,
+    )
+    step_process = ProcessingStep(
+        name="PreprocessDataset",
+        processor=sklearn_processor,
+        outputs=[
+            ProcessingOutput(output_name="train", source="/opt/ml/processing/train"),
+            ProcessingOutput(output_name="validation", source="/opt/ml/processing/validation"),
+            ProcessingOutput(output_name="test", source="/opt/ml/processing/test"),
+        ],
+        code=os.path.join(BASE_DIR, "preprocess.py"),
+        job_arguments=["--input-data", input_data],
+    )
+
+    # training step for generating model artifacts
+    model_path = f"s3://{sagemaker_session.default_bucket()}/{base_job_prefix}/scam-detect-train"
+    image_uri = sagemaker.image_uris.retrieve(
+        framework="xgboost",
+        region=region,
+        version="1.0-1",
+        py_version="py3",
+        instance_type=training_instance_type,
+    )
+    xgb_train = Estimator(
+        image_uri=image_uri,
+        instance_type=training_instance_type,
+        instance_count=1,
+        output_path=model_path,
+        base_job_name=f"{base_job_prefix}/train",
+        sagemaker_session=sagemaker_session,
+        role=role,
+    )
+    xgb_train.set_hyperparameters(
+        objective="binary:logistic",
+        num_round=500,
+        gamma=0,
+        max_depth=3
+    )
+    step_train = TrainingStep(
+        name="TrainModel",
+        estimator=xgb_train,
+        inputs={
+            "train": TrainingInput(
+                s3_data=step_process.properties.ProcessingOutputConfig.Outputs[
+                    "train"
+                ].S3Output.S3Uri,
+                content_type="text/csv",
+            ),
+            "validation": TrainingInput(
+                s3_data=step_process.properties.ProcessingOutputConfig.Outputs[
+                    "validation"
+                ].S3Output.S3Uri,
+                content_type="text/csv",
+            ),
+        },
+    )
+
+    # processing step for evaluation
+    script_eval = ScriptProcessor(
+        image_uri=image_uri,
+        command=["python3"],
+        instance_type=processing_instance_type,
+        instance_count=1,
+        base_job_name=f"{base_job_prefix}/evaluate",
+        sagemaker_session=sagemaker_session,
+        role=role,
+    )
+    evaluation_report = PropertyFile(
+        name="ScamDetectEvaluationReport",
+        output_name="evaluation",
+        path="evaluation.json",
+    )
+    step_eval = ProcessingStep(
+        name="EvaluateModel",
+        processor=script_eval,
+        inputs=[
+            ProcessingInput(
+                source=step_train.properties.ModelArtifacts.S3ModelArtifacts,
+                destination="/opt/ml/processing/model",
+            ),
+            ProcessingInput(
+                source=step_process.properties.ProcessingOutputConfig.Outputs[
+                    "test"
+                ].S3Output.S3Uri,
+                destination="/opt/ml/processing/test",
+            ),
+        ],
+        outputs=[
+            ProcessingOutput(output_name="evaluation", source="/opt/ml/processing/evaluation"),
+        ],
+        code=os.path.join(BASE_DIR, "evaluate.py"),
+        property_files=[evaluation_report],
+    )
+
+    # create the model
+    model = Model(
+        image_uri=image_uri,
+        model_data=step_train.properties.ModelArtifacts.S3ModelArtifacts,
+        sagemaker_session=sagemaker_session,
+        role=role,
+        predictor_cls=XGBoostPredictor,
+    )
+
+    step_create_model = CreateModelStep(
+        name="CreateModel",
+        model=model,
+        inputs=sagemaker.inputs.CreateModelInput(instance_type="ml.m5.large"),
+    )
+
+    # register model step that will be conditionally executed
+    model_metrics = ModelMetrics(
+        model_statistics=MetricsSource(
+            s3_uri="{}/evaluation.json".format(
+                step_eval.arguments["ProcessingOutputConfig"]["Outputs"][0]["S3Output"]["S3Uri"]
+            ),
+            content_type="application/json"
+        )
+    )
+    step_register = RegisterModel(
+        name="RegisterModel",
+        estimator=xgb_train,
+        model_data=step_train.properties.ModelArtifacts.S3ModelArtifacts,
+        content_types=["text/csv"],
+        response_types=["text/csv"],
+        inference_instances=["ml.m5.xlarge", "ml.m5.2xlarge"],
+        transform_instances=["ml.m5.xlarge"],
+        model_package_group_name=model_package_group_name,
+        approval_status=model_approval_status,
+        model_metrics=model_metrics,
+    )
+
+    # deploy endpoint via Lambda step
+    lambda_role = role
+    current_time = time.strftime("%m-%d-%H-%M-%S", time.localtime())
+    model_name = "scam-detect-model" + current_time
+    endpoint_config_name = "scam-detect-endpoint-config"
+    endpoint_name = "scam-detect-endpoint"
+    function_name = "sagemaker-lambda-step-endpoint-deploy"
+    
+    # Lambda helper class can be used to create the Lambda function
+    func = Lambda(
+        function_name=function_name,
+        execution_role_arn=lambda_role,
+        script=os.path.join(BASE_DIR, "lambda_deploy.py"),
+        handler="lambda_deploy.lambda_handler",
+    )
+    
+    output_param_1 = LambdaOutput(output_name="statusCode", output_type=LambdaOutputTypeEnum.String)
+    output_param_2 = LambdaOutput(output_name="body", output_type=LambdaOutputTypeEnum.String)
+    output_param_3 = LambdaOutput(output_name="other_key", output_type=LambdaOutputTypeEnum.String)
+    
+    step_deploy_lambda = LambdaStep(
+        name="DeployEndpoint",
+        lambda_func=func,
+        inputs={
+            "model_name": step_create_model.properties.ModelName,
+            "endpoint_config_name": endpoint_config_name,
+            "endpoint_name": endpoint_name,
+        },
+        outputs=[output_param_1, output_param_2, output_param_3],
+    )
+
+    # condition step for evaluating model quality and branching execution
+    cond_lte = ConditionLessThanOrEqualTo(
+        left=JsonGet(
+            step=step_eval,
+            property_file=evaluation_report,
+            json_path="regression_metrics.mse.value"
+        ),
+        right=6.0,
+    )
+    step_cond = ConditionStep(
+        name="CheckEvaluation",
+        conditions=[cond_lte],
+        if_steps=[step_create_model, step_deploy_lambda, step_register],
+        else_steps=[],
+    )
+
+    # pipeline instance
+    pipeline = Pipeline(
+        name=pipeline_name,
+        parameters=[
+            processing_instance_type,
+            processing_instance_count,
+            training_instance_type,
+            model_approval_status,
+            input_data,
+        ],
+        steps=[step_process, step_train, step_eval, step_cond],
+        sagemaker_session=sagemaker_session,
+    )
+    return pipeline
